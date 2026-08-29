e2fa1c1a1999850392fa37a93def9635b4bf66d7
Piotr Wendzierski <piotrwendzierski@gmail.com>
2026-08-29 11:56:52 +0200

    3. Parse JSON (object + array) and list genres via foreach

--- index.php
+++ index.php
@@ -54,5 +54,14 @@ if ($err) {
 
   if($adult_array == false) echo "<br>"."adult = false"."<br>";
   else echo $adult_array;
+
+
+  // echo genres from movie 11 - foreach
+  echo "3. Genres"."<br>";
+  //print_r($data_array['genres']);
+  foreach($data_array['genres'] as $gatunek){
+    echo "genre id: ". $gatunek['id']. " ". "genre name: " .$gatunek['name']."<br>";
+  }
+
 }
 ?>
